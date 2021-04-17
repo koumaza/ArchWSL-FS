e8df846c850028fbea3c2057ab11b2b86e1217af
shanghai yakisoba chan! <koumaza--@outlook.com>
2021-04-18 01:52:32 +0900

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ARCH="x86_64"
-ROOTFS_VER="2020.10.01"
+ROOTFS_VER="2021.04.01"
 ROOTFS_FN="archlinux-bootstrap-${ROOTFS_VER}-${ARCH}.tar.gz"
-ROOTFS_URL="http://mirrors.kernel.org/archlinux/iso/${ROOTFS_VER}/${ROOTFS_FN}"
+ROOTFS_URL="https://mirrors.edge.kernel.org/archlinux/iso/${ROOTFS_VER}/${ROOTFS_FN}"
 
 FRTCP_BLD="18082100"
 FRTCP_VER="1.23-1"
@@ -12,14 +12,10 @@ FRTCP_URL="https://github.com/yuk7/arch-prebuilt/releases/download/${FRTCP_BLD}/
 
 PAC_PKGS="base less nano sudo vim curl"
 
-if type curl >/dev/null 2>&1 ;then
+if type aria2c >/dev/null 2>&1 ; then
+    DLR="aria2c -x16 -s20"
+elif type wget >/dev/null 2>&1 ; then
+    DLR="wget"
+elif type curl >/dev/null 2>&1 ; then
     DLR="curl"
 fi
-if type wget >/dev/null 2>&1;then
-    DLR="wget"
-fi
-if type aria2c >/dev/null 2>&1; then
-    DLR="aria2c -x4"
-fi
-
-
